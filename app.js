63689c28d48ca74d0c0b25a29f0f10bcdb6598ff
Andrei Danila <115635392+bigsosa337@users.noreply.github.com>
2023-09-18 00:16:02 +0300

    route to download image&details bugfix

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
 });
 
-const {storage} = require("./database"); // Import the Firebase Storage instance
+const {storage} = require("./database"); // import the Firebase Storage instance
 const storageRef = storage;
 
 const upload = multer({
-    storage: multer.memoryStorage(), // Use in-memory storage for handling file uploads
+    storage: multer.memoryStorage(), // Uue in-memory storage for handling file uploads
 });
 
 
@@ -37,19 +37,21 @@ app.post('/uploadImage', upload.single('image'), async (req, res) => {
             return res.status(400).json({ error: "No image file selected." });
         }
 
-        // Generate a unique filename for the uploaded file
-        const filename = `images/${Date.now()}-${req.file.originalname}`;
+        let filename = `images/${Date.now()}-${req.file.originalname}`;
+        filename = filename.split('.')
+        filename.pop();
+        console.log('-------------------------')
+        console.log(filename, typeof filename)
+        console.log('-------------------------')
+        filename = filename.join(".");
 
-        // Extract tags and description from the request body
         const tags = Array.isArray(req.body.tags) ? req.body.tags : [];
-        const description = req.body.description || ''; // Set a default empty string if description is not provided
-        const title = req.body.title || ''; // Set a default empty string if title is not provided
+        const description = req.body.description || '';
+        const title = req.body.title || '';
 
-        // Upload the file to Firebase Storage
         const file = storage.bucket().file(filename);
         await file.save(req.file.buffer);
 
-        // Store image details and metadata in Firestore
         const imageDetails = {
             filename: filename,
             title: title,
@@ -71,10 +73,8 @@ app.post('/uploadImage', upload.single('image'), async (req, res) => {
 //this function gets all the images from the database
 app.get('/listImages', async (req, res) => {
     try {
-        // List all files in the 'images' directory
         const [files] = await storage.bucket().getFiles({prefix: 'images/'});
 
-        // Extract the filenames from the list of files
         const filenames = files.map((file) => file.name.split('/').pop());
 
         res.json({images: filenames});
@@ -91,20 +91,16 @@ app.get('/getImage/:filename', async (req, res) => {
         const filename = req.params.filename;
         const file = storage.bucket().file(`images/${filename}`);
 
-        // Check if the file exists in Firebase Storage
         const [fileExists] = await file.exists();
 
         if (!fileExists) {
             return res.status(404).json({ error: "Image not found." });
         }
 
-        // Fetch the metadata associated with the image
         const [metadata] = await file.getMetadata();
 
-        // Set the response content type based on the file type
         res.setHeader('Content-Type', 'image/jpeg'); // Adjust the content type as needed
 
-        // Create a response object that includes both the metadata and the image data
         const response = {
             metadata,
             imageStream: file.createReadStream(),
@@ -122,14 +118,12 @@ app.get('/getImageDetails/:filename', async (req, res) => {
         const filename = req.params.filename;
         const file = storage.bucket().file(`images/${filename}`);
 
-        // Check if the file exists in Firebase Storage
         const [fileExists] = await file.exists();
 
         if (!fileExists) {
             return res.status(404).json({ error: "Image not found." });
         }
 
-        // Query Firestore to get the image's metadata
         const imageMetadata = await db.db.collection("images")
             .where("filename", "==", `images/${filename}`)
             .get();
@@ -146,6 +140,117 @@ app.get('/getImageDetails/:filename', async (req, res) => {
         res.json(metadata);
     } catch (error) {
         console.error("Error retrieving image:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get('/getImageWithDetails/:filename', async (req, res) => {
+    try {
+        const filename = req.params.filename;
+        const file = storage.bucket().file(`images/${filename}`);
+
+        const [fileExists] = await file.exists();
+
+        if (!fileExists) {
+            return res.status(404).json({ error: "Image not found." });
+        }
+
+        const [metadata] = await file.getMetadata();
+
+        const imageMetadata = await db.db.collection("images")
+            .where("filename", "==", `images/${filename}`)
+            .get();
+
+        if (imageMetadata.empty) {
+            return res.status(404).json({ error: "Image metadata not found." });
+        }
+
+        const firestoreMetadata = imageMetadata.docs[0].data();
+
+        const combinedMetadata = {
+            ...metadata,
+            ...firestoreMetadata,
+        };
+
+        combinedMetadata.imageUrl = file.publicUrl();
+
+        res.json(combinedMetadata);
+    } catch (error) {
+        console.error("Error retrieving image:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get('/getImageData/:filename', async (req, res) => {
+    try {
+        const filename = req.params.filename;
+        const file = storage.bucket().file(`images/${filename}`);
+
+        const [fileExists] = await file.exists();
+
+        if (!fileExists) {
+            return res.status(404).json({ error: "Image not found." });
+        }
+
+        const fileStream = file.createReadStream();
+
+        res.setHeader('Content-Type', 'image/jpeg'); // Adjust the content type as needed
+
+        fileStream.pipe(res);
+    } catch (error) {
+        console.error("Error retrieving image data:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get('/getImage1/:filename', async (req, res) => {
+    try {
+        const filename = req.params.filename;
+        const file = storage.bucket().file(`images/${filename}`);
+
+        const [fileExists] = await file.exists();
+
+        if (!fileExists) {
+            return res.status(404).json({ error: "Image not found." });
+        }
+
+        const fileStream = file.createReadStream();
+
+        res.setHeader('Content-Type', 'image/jpeg'); // Adjust the content type as needed
+
+        fileStream.pipe(res);
+    } catch (error) {
+        console.error("Error retrieving image:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.get('/getImageInfo/:filename', async (req, res) => {
+    try {
+        const filename = req.params.filename;
+
+        const querySnapshot = await db.db.collection("images")
+            .where("filename", "==", `images/${filename}`)
+            .get();
+        console.log(querySnapshot, `images/${filename}`)
+        if (querySnapshot.empty) {
+            // If no matching documents found, send a 404 response
+            return res.status(404).json({ error: "Image not found." });
+        }
+
+        const imageInfo = querySnapshot.docs[0].data();
+
+        const imageInfoResponse = {
+            filename: imageInfo.filename,
+            title: imageInfo.title,
+            description: imageInfo.description,
+            tags: imageInfo.tags,
+            // Add other fields as needed
+        };
+
+        res.json({ imageInfo: imageInfoResponse });
+    } catch (error) {
+        console.error("Error fetching image information:", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
